6846e06de0e34fb909649068d08b114e248e8dc2
Adam Woodruff <atw3710@gmail.com>
2022-05-05 21:04:25 -0400

    Keep forgetting to git add xD

---
+++ Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Text_Based_RPG_Epic_Edition
+{
+    public class Item
+    {
+        public string Name { get; set; } = "";
+        public int Damage { get; set; } = 0;
+        public double Bonus { get; set; } = 1.50; // interpret as a multiplier
+
+        public void DefineItemVars()
+        {
+
+        }
+    }
+}
---
+++ Items/ExampleItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic_Text_Based_RPG_Epic_Edition.Items
+{
+    public class ExampleItem : Item
+    {
+        new public void DefineItemVars()
+        {
+            Name = "Example Item";
+            Damage = 30;
+            Bonus = 1.3;
+        }
+    }
+}
