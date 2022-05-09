3c610d6de3acf42f22ed57e2f800923d09308d4a
Adam Woodruff <atw3710@gmail.com>
2022-05-09 12:20:24 -0400

    WHY DO I KEEP FORGETTING THE GIT ADD

---
+++ SaveVarStorage.cs
@@ -0,0 +1,25 @@
+﻿using Generic_Text_Based_RPG_Epic_Edition.BaseClasses;
+using System.Text.Json.Serialization;
+
+namespace Generic_Text_Based_RPG_Epic_Edition
+{
+    public struct SaveVarStorage
+    {
+        public Player Player { get; set; }
+        public int PlayerWeaponID { get; set; }
+        public EnemyStatsStruct EnemyStats { get; set; }
+        public int EnemyID { get; set; }
+
+        [JsonIgnore]
+        public Enemy Enemy { get; set; }
+    }
+
+    public struct EnemyStatsStruct
+    {
+        public int Power { get; set; }
+        public int Health { get; set; }
+        public int Defense { get; set; }
+        public int CoinBonus { get; set; }
+        public int XP { get; set; }
+    }
+}
